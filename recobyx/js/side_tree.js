7cdc586f65f8563d10b01aea1d92d88e5e9b2569
recobyx <s13xit+recobyx@disroot.org>
2019-06-07 18:28:10 +0900

    highlight added

--- recobyx/js/side_tree.js
+++ recobyx/js/side_tree.js
@@ -4,8 +4,34 @@
   let anchors = document.getElementsByTagName('a');
   for (let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener('click', function(e) {
+      console.log("clicked: "+ e.target.id);
+      let src_nav = e.target.id.split(/#/);
+      let nav = src_nav.length == 1 ? "" : src_nav[1];
       let ifr = window.parent.document.getElementById('content_iframe');
-      ifr.setAttribute('src', e.target.id +'.html');
+      ifr.addEventListener('load', function() {
+        console.log("ifr load: "+ src_nav);
+        if (nav) {
+          console.log(nav);
+          let elem = ifr.contentDocument.getElementById(nav);
+          elem.setAttribute('class', 'highlight');
+        }
+      });
+      ifr.setAttribute('src', src_nav[0] +'.html#'+ nav);
+
+      if (nav) {
+        let doc = ifr.contentDocument;
+        let coll = doc.querySelectorAll('[class=highlight]');
+        console.log("coll: "+ coll.length);
+        for (let j = 0; j < coll.length; j++) {
+          console.log("now hl> "+ coll[j].id);
+          coll[j].removeAttribute('class', 'highlight');
+        }
+
+        let elem = doc.getElementById(nav);
+        if (elem) {
+          elem.setAttribute('class', 'highlight');
+        }
+      }
     }, false);
   }
 
